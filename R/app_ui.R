17d76a66becfe2a749f780739d6e31e064f63c04
Kale-23 <kalebduch18@gmail.com>
2025-10-06 14:48:20 -0400

    working with card and dark mode

--- R/app_ui.R
+++ R/app_ui.R
@@ -28,32 +28,59 @@ app_ui <- function(request) {
         flex-direction: column;
         height: 100vh;
         ",
-
         bslib::card(
-          height = "8vh",
-          shiny::titlePanel(
-            title = "Snow and Frost data viewer",
-            windowTitle = "Snow and Frost Data Explorer"
-          )
-        ),
+          bslib::card_header(
+            # title and tooltip
+            shiny::div(
+              style = "display: flex; justify-content: space-between; align-items: center;",
 
-        #TODO fix this
-        mod_global_ui("global_1", tot_height = "20vh"),
+              shiny::span(
+                shiny::h3(
+                  style = "display: inline; margin-right: 0.3em;",
+                  "Snow Pits and Frost Tubes Dashboard"
+                ),
+                bslib::tooltip(
+                  bsicons::bs_icon("info-circle"),
+                  # intro text
+                  "Visualize and download data and graphs from the Snow Pits and Frost Tubes datasets. These datasets are collected and updated daily during the snow season",
+                ),
+              ),
 
-        bslib::navset_card_pill(
-          height = "80vh",
-          placement = "above",
-          bslib::nav_panel(
-            title = "Frost",
-            mod_gen_ui("frost_1") # from mod_frost.R
+              # source code link
+              shiny::div(
+                # Source code link
+                shiny::a(
+                  href = "https://github.com/Kale-23/soil_dashboard.git", # <-- update this link
+                  target = "_blank",
+                  bslib::tooltip(
+                    bsicons::bs_icon("github"),
+                    "View source code on GitHub"
+                  )
+                ),
+              ),
+
+              # theme toggle
+              bslib::input_dark_mode()
+            )
           ),
-          bslib::nav_panel(
-            title = "Pits",
-            mod_gen_ui("pits_1") # from mod_pits.R
+          #TODO fix this
+          mod_global_ui("global_1", tot_height = "20vh"),
+
+          bslib::navset_card_pill(
+            height = "80vh",
+            placement = "above",
+            bslib::nav_panel(
+              title = "Frost",
+              mod_gen_ui("frost_1") # from mod_frost.R
+            ),
+            bslib::nav_panel(
+              title = "Pits",
+              mod_gen_ui("pits_1") # from mod_pits.R
+            )
           )
         )
+        #footer,
       )
-      #footer,
     )
   )
 }
